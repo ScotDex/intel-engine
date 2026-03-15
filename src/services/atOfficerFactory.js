36cf6f00d83d89b2902b5810ee021cc0d23779c3
ScotDex <themadlyscientific@gmail.com>
2026-03-15 16:28:43 +0000

    more testing

--- src/services/atOfficerFactory.js
+++ src/services/atOfficerFactory.js
@@ -4,9 +4,13 @@ const helpers = require('../core/helpers')
 class atOfficerFactory {
     static createKillEmbed(kill, zkb, names) {
         const DOTLAN_BASE = 'https://evemaps.dotlan.net'
-        const totalValue = helpers.formatIsk(zkb.totalValue)
         const corpIcon = `https://edge.socketkill.com/taylr/logo.png`;
         const title = 'Activity Detected';
+
+        const triggerAttacker = kill.attackers?.find(a => 
+            AT_SHIP_IDS.has(a.ship_type_id) || OFFICER_SHIP_IDS.has(a.ship_type_id)
+        );
+
 
         return {
             username: "Officer/AT Ship Alarm (Beta)",
@@ -14,18 +18,17 @@ class atOfficerFactory {
             embeds: [{
                 author: {
                     name: `${names.triggerShipName} spotted in ${names.systemName}`,
-                    icon_url: `https://images.evetech.net/characters/${names.triggerShipId}/portrait?size=128`
+                    icon_url: `https://images.evetech.net/corporations/${triggerAttacker?.corporation_id}/logo?size=64`
                 },
                 title: title,
                 url: `https://zkillboard.com/kill/${kill.killmail_id}/`,
-                thumbnail: { url: `https://images.evetech.net/types/${kill.victim.ship_type_id}/render?size=256` },
+                thumbnail: { url: `https://images.evetech.net/types/${names.triggerShipId}/render?size=256` },
                 color: 0xf39c12,
                 fields: [
                     { name: "System", value: `** [${names.systemName}](${DOTLAN_BASE}/system/${names.systemName.replace(/ /g, '_')}) ** `, inline: false },
                     { name: "Region", value: `** [${names.regionName}](${DOTLAN_BASE}/region/${names.regionName.replace(/ /g, '_')}) ** `, inline: false },
                     { name: "Pilot", value: names.triggerCharName, inline: false },
                     { name: "Corporation", value: names.triggerCorpName, inline: false },
-                    { name: "Total Value", value: `**${totalValue} ISK**`, inline: false },
                 ],
                 footer: {
                     text: `Powered by socketkill.com`,
